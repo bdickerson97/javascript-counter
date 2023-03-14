58726dd294d67f35947c30f4ce71d46955973f79
Michael Dickerson <bdickerson97@yahoo.com>
2023-03-14 15:39:09 -0400

    Change button group labels for readability I changed the button-container element from class to id, and I unified each of the buttons by making them of the same class.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const INCREASE = document.getElementById("increase");
+const RESET = document.getElementById("reset");
+const DECREASE = document.getElementById("decrease");
+
+INCREASE.addEventListener("click", () => {console.log("increase")});
+
+
+
+
+
+
+
+
+
